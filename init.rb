02e35ac6fe184388ecc2879a9a23c3ce36aea9cb
Dmitry <don.iliy@gmail.com>
2012-04-02 12:53:37 +0300

    updade mongo db connection

--- init.rb
+++ init.rb
@@ -5,8 +5,19 @@ require 'mongoid'
 require 'csv'
 require 'date'
 
-require File.expand_path(File.dirname(__FILE__) + '/models/db_mongo')
-Mongoid.load!("config/mongoid.yml")
+#require File.expand_path(File.dirname(__FILE__) + '/models/db_mongo')
+#Mongoid.load!("config/mongoid.yml")
+
+ Mongoid.configure do |config|
+    if ENV['MONGOHQ_URL']
+      conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+      uri = URI.parse(ENV['MONGOHQ_URL'])
+      config.master = conn.db(uri.path.gsub(/^\//, ''))
+    else
+      config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('checkinn_development')
+    end
+  end
+
 
 class CheckinnApi < Sinatra::Base
 	
